fa5ab8d6ae59533299b0e199153534c412328658
despla_s <simon.desplat@epitech.eu>
2018-04-09 13:21:08 -0400

    fiche film avec style très léger ok

--- page.py
+++ page.py
@@ -20,15 +20,44 @@ def main():
         films[i]['moyenne'] = Tuple[2]
         films[i]['synopsis'] = Tuple[3]
         i += 1
-    cur.close()
     return render_template('index.html', films=films)
 
 @app.route("/film/<film_id>")
 def film_page(film_id):
-    # Je n'ai pas encore changé le template juste pour éviter un plantage en cliquant sur les liens
-    # de la page d'accueil.
-    # Changer template pour film.html et changer les variables de template associé
-    return render_template('user.html', username=film_id)
+    r1 = "SELECT titre_film, DATE_FORMAT(date_parution, '%D %M %Y'), duree, note_moyenne, genre, synopsis FROM film WHERE id_film=" + film_id + ";"
+    cur = conn.cursor()
+    cur.execute(r1)
+    film_info = {}
+    for Tuple in cur:
+        film_info['titre'] = Tuple[0]
+        film_info['date'] = Tuple[1]
+        film_info['duree'] = Tuple[2]
+        film_info['note'] = Tuple[3]
+        film_info['genre'] = Tuple[4]
+        film_info['synopsis'] = Tuple[5]
+
+    r2 = "SELECT jouer.id_acteur AS id_acteur, nom_acteur FROM jouer JOIN acteur ON jouer.id_acteur=acteur.id_acteur WHERE jouer.id_film=" + film_id + ";"
+    cur.execute(r2)
+    acteurs = []
+    i = 0
+    for Tuple in cur:
+        acteurs.append({})
+        acteurs[i]['acteur_url'] = "/acteur/" + str(Tuple[0])
+        acteurs[i]['nom'] = Tuple[1]
+        i += 1
+
+    r3 = "SELECT creer.id_realisateur AS id_realisateur, nom_realisateur FROM creer JOIN realisateur ON creer.id_realisateur=realisateur.id_realisateur WHERE creer.id_film=" + film_id + ";"
+    cur.execute(r3)
+    realisateurs = []
+    i = 0
+    for Tuple in cur:
+        realisateurs.append({})
+        realisateurs[i]['realisateur_url'] = "/realisateur/" + str(Tuple[0])
+        realisateurs[i]['nom'] = Tuple[1]
+        i += 1
+    cur.close()
+
+    return render_template('film.html', film=film_info, acteurs=acteurs, realisateurs=realisateurs)
 
 @app.route("/user/<user_id>")
 def user_page(user_id):
