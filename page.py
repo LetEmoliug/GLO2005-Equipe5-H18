3f6d6a8a0d3d2fe65f0ca8dc827544f23f8c37c4
despla_s <simon.desplat@epitech.eu>
2018-04-09 12:19:04 -0400

    added little css for homepage + info on README + little film route for test

--- page.py
+++ page.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    requete = "SELECT titre_film, note_moyenne, synopsis FROM film WHERE note_moyenne > 7.5 LIMIT 5;"
+    requete = "SELECT id_film, titre_film, note_moyenne, CONCAT(LEFT(synopsis, 330), '...') FROM film WHERE note_moyenne > 7.5 ORDER BY RAND() LIMIT 5;"
     cur = conn.cursor()
     cur.execute(requete)
 
@@ -15,15 +15,23 @@ def main():
     i = 0
     for Tuple in cur:
         films.append({})
-        films[i]['titre'] = Tuple[0]
-        films[i]['moyenne'] = Tuple[1]
-        films[i]['synopsis'] = Tuple[2]
+        films[i]['film_url'] = "/film/" + str(Tuple[0])
+        films[i]['titre'] = Tuple[1]
+        films[i]['moyenne'] = Tuple[2]
+        films[i]['synopsis'] = Tuple[3]
         i += 1
     cur.close()
     return render_template('index.html', films=films)
 
+@app.route("/film/<film_id>")
+def film_page(film_id):
+    # Je n'ai pas encore changé le template juste pour éviter un plantage en cliquant sur les liens
+    # de la page d'accueil.
+    # Changer template pour film.html et changer les variables de template associé
+    return render_template('user.html', username=film_id)
+
 @app.route("/user/<user_id>")
-def u(user_id):
+def user_page(user_id):
     return render_template('user.html', username=user_id)
 
 
