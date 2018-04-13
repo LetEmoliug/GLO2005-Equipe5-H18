5279116239c69c53f4dd2685ebad48e883f503b2
jeromebergeron1 <Special3>
2018-04-13 11:37:03 -0400

    barre de recherche completer

--- page.py
+++ page.py
@@ -68,7 +68,58 @@ def user_page(user_id):
 @app.route("/ResultatRecherche", methods=['POST'])
 def ResultatsRecherche():
     recherche = request.form.get('recherche')
-    requete = "SELECT id_film, titre_film, note_moyenne, CONCAT(LEFT(synopsis, 330), '...') FROM film WHERE titre_film LIKE '%" + recherche + "%';"
+    requete_films = "SELECT id_film, titre_film FROM film WHERE titre_film LIKE '%" + recherche + "%';"
+    cur = conn.cursor()
+    cur.execute(requete_films)
+
+    film = []
+    i = 0
+    for Tuple in cur:
+        film.append({})
+        film[i]['film_url'] = "/film/" + str(Tuple[0])
+        film[i]['titre'] = Tuple[1]
+        i += 1
+
+    requete_acteur = "SELECT id_acteur, nom_acteur FROM acteur WHERE nom_acteur LIKE '%" + recherche + "%';"
+    cur = conn.cursor()
+    cur.execute(requete_acteur)
+
+    acteur = []
+    i = 0
+    for Tuple in cur:
+        acteur.append({})
+        acteur[i]['acteur_url'] = "/acteur/" + str(Tuple[0])
+        acteur[i]['nom'] = Tuple[1]
+        i += 1
+
+    requete_realisateur = "SELECT id_realisateur, nom_realisateur FROM realisateur WHERE nom_realisateur LIKE '%" + recherche + "%';"
+    cur = conn.cursor()
+    cur.execute(requete_realisateur)
+
+    realisateur = []
+    i = 0
+    for Tuple in cur:
+        realisateur.append({})
+        realisateur[i]['realisateur_url'] = "/realisateur/" + str(Tuple[0])
+        realisateur[i]['nom'] = Tuple[1]
+        i += 1
+
+    requete_utilisateur = "SELECT nom_usager FROM utilisateur WHERE nom_usager LIKE '%" + recherche + "%';"
+    cur = conn.cursor()
+    cur.execute(requete_utilisateur)
+
+    utilisateur = []
+    i = 0
+    for Tuple in cur:
+        utilisateur.append({})
+        utilisateur[i]['utilisateur_url'] = "/utilisateur/" + str(Tuple[0])
+        utilisateur[i]['nom'] = Tuple[0]
+        i += 1
+    return render_template('ResultatRecherche.html', film=film, acteur=acteur, realisateur=realisateur, utilisateur=utilisateur)
+
+@app.route("/Films")
+def Films():
+    requete = "SELECT id_film, titre_film, note_moyenne, CONCAT(LEFT(synopsis, 330), '...') FROM film;"
     cur = conn.cursor()
     cur.execute(requete)
 
@@ -81,8 +132,7 @@ def ResultatsRecherche():
         films[i]['moyenne'] = Tuple[2]
         films[i]['synopsis'] = Tuple[3]
         i += 1
-    return render_template('ResultatRecherche.html', films=films)
-
+    return render_template('films.html', films=films)
 
 if __name__ == "__main__":
     app.run()
