381488752baeb53cc0d82efaef681237398e54ba
Felipe9009 <37447403+Felipe9009@users.noreply.github.com>
2018-04-14 00:02:25 -0400

    ...

--- Afficher_critiques.py
+++ Afficher_critiques.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, abort, request
 import pymysql
 import pymysql.cursors
 
-conn = pymysql.connect(host='localhost', user="root", password="root", db="projetsession")
+conn = pymysql.connect(host='localhost', user="root", password="012345", db="tpbd")
 app = Flask(__name__)
 
 VarGlobal = {}
@@ -116,7 +116,66 @@ def ResultatsRecherche():
         i += 1
     return render_template('ResultatRecherche.html', films=films)
 
+@app.route("/realisateur/<id_realisateur>")
 
+def page_realisateur(id_realisateur):
+    #Requête info realisateur.
+    req = "SELECT r.nom_realisateur, DATE_FORMAT(r.date_naissance, '%D %M %Y'), r.pays_origine, r.sexe, r.biographie FROM realisateur r WHERE r.id_realisateur = " + id_realisateur +";"
+           #SELECT r.nom_realisateur, DATE_FORMAT(r.date_naissance, '%D %M %Y'), r.pays_origine, r.sexe, r.biographie FROM realisateur r WHERE r.id_realisateur = 23;
+    cur = conn.cursor()
+    cur.execute(req)
+
+    info_realisateur = {}
+    for Tuple in cur:
+        info_realisateur['nom'] = Tuple[0]
+        info_realisateur['date'] = Tuple[1]
+        info_realisateur['pays_origine'] = Tuple[2]
+        info_realisateur['sexe'] = Tuple[3]
+        info_realisateur['biographie'] = Tuple[4]
+
+    #Requête titres des films que le realisateur a crée.
+    req2 = "SELECT f.titre_film FROM film f INNER JOIN creer c ON f.id_film = c.id_film WHERE c.id_realisateur = " + id_realisateur + ";"
+    cur.execute(req2)
+
+
+    liste_filmes_crees = []
+    for Tuple in cur:
+        liste_filmes_crees.append(Tuple)
+
+    cur.close()
+
+    return render_template('realisateur.html', realisateur = info_realisateur, liste = liste_filmes_crees)
+
+@app.route("/acteur/<id_acteur>")
+def page_acteur(id_acteur):
+    #Requête info acteur.
+    req = "SELECT a.nom_acteur, DATE_FORMAT(a.date_naissance, '%D %M %Y'), a.pays_origine, a.sexe, a.biographie FROM acteur a WHERE a.id_acteur = "+id_acteur+";"
+
+    cur = conn.cursor()
+    cur.execute(req)
+
+    info_acteur = {}
+    for Tuple in cur:
+        info_acteur['nom'] = Tuple[0]
+        info_acteur['date'] = Tuple[1]
+        info_acteur['pays_origine'] = Tuple[2]
+        info_acteur['sexe'] = Tuple[3]
+        info_acteur['biographie'] = Tuple[4]
+
+    #Requête titres des films ou l'acteur a participé.
+    req2 = "SELECT f.titre_film, f.id_film FROM film f INNER JOIN jouer j ON f.id_film = j.id_film WHERE j.id_acteur =" + id_acteur + ";"
+    cur.execute(req2)
+
+    dictionaire_film = {}
+
+    for film, id in cur:
+        if id not in dictionaire_film:
+            dictionaire_film[film] = [id]
+        else:
+            dictionaire_film[film].append(id)
+    cur.close()
+
+    return render_template('acteur.html', acteur=info_acteur, liste=dictionaire_film)
 
 if __name__ == "__main__":
     app.run()
