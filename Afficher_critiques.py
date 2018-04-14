87212ff651640673e3ebe0fc9b3458a3a4e7df2e
Felipe9009 <37447403+Felipe9009@users.noreply.github.com>
2018-04-14 02:06:26 -0400

    Merge branch 'master' of https://github.com/LetEmoliug/GLO2005-Equipe5-H18

--- Afficher_critiques.py
+++ Afficher_critiques.py
@@ -116,7 +116,36 @@ def ResultatsRecherche():
         i += 1
     return render_template('ResultatRecherche.html', films=films)
 
+@app.route("/acteur/<id_acteur>")
+def page_acteur(id_acteur):
+    #Requête info acteur.
+    req = "SELECT a.nom_acteur, DATE_FORMAT(a.date_naissance, '%D %M %Y'), a.pays_origine, a.sexe, a.biographie FROM acteur a WHERE a.id_acteur ="+id_acteur+";"
 
+    cur = conn.cursor()
+    cur.execute(req)
+
+    info_acteur = {}
+    for Tuple in cur:
+        info_acteur['nom'] = Tuple[0]
+        info_acteur['date'] = Tuple[1]
+        info_acteur['pays_origine'] = Tuple[2]
+        info_acteur['sexe'] = Tuple[3]
+        info_acteur['biographie'] = Tuple[4]
+
+    #Requête titres des films ou l'acteur a participé.
+    req2 = "SELECT f.titre_film, f.id_film FROM film f INNER JOIN jouer j ON f.id_film = j.id_film WHERE j.id_acteur =" + id_acteur + ";"
+    cur.execute(req2)
+
+    liste_filmes = []
+    i = 0
+    for Tuple in cur:
+        liste_filmes.append({})
+        liste_filmes[i]['film'] = Tuple[0]
+        liste_filmes[i]['film_url'] = "/film/" + str(Tuple[1])
+        i += 1
+    cur.close()
+
+    return render_template('acteur.html', acteur = info_acteur, liste = liste_filmes)
 
 if __name__ == "__main__":
     app.run()
