5a86e2a67014caf19d4a01e284ca89d792f3b059
jeromebergeron1 <Special3>
2018-04-16 11:51:31 -0400

    filtes non fonctionnels

---
+++ static/js/films.js
@@ -0,0 +1,63 @@
+var itemsToFilter = document.querySelectorAll("#itemsToFilter li");
+
+      var checkBoxes = document.querySelectorAll(".filterSection li input");
+
+      for (var i = 0; i < checkBoxes.lengt; i++) {
+          checkBoxes[i].addEventListener("click", filterItems, false);
+          checkBoxes[i].checked = true;
+      }
+
+      function filterItems(e) {
+          var clickedItem = e.target;
+
+          if (clickedItem.checked == true) {
+              hideOrShowItems (clickedItem.value, "hideItem", "showItem");
+
+          } else if (clickedItem.checked == false) {
+              hideOrShowItems (clickedItem.value, "showItem", "hideItem");
+
+          }
+      }
+
+      function hideOrShowItems(itemType, classToRemove, classToAdd) {
+          for (var i = 0; i < itemsToFilter.length; i++) {
+              var currentItem = itemsToFilter[i];
+
+              if (currentItem.getAttribure("data-type") == itemType) {
+                  removeClass(currentItem, classToRemove);
+                  addClass(currentItem, classToAdd);
+              }
+          }
+      }
+
+      function addClass(element, classToAdd) {
+          var currentClassValue = element.className;
+
+          if (currentClassValue.indexOf(classToAdd) == -1) {
+              if ((currentClassValue == null) || currentClassValue === "")) {
+                  element.className = classToAdd;
+              } else {
+                  element.className += " " + classToAdd;
+              }
+          }
+      }
+
+      function removeClass(element, classToRemove) {
+          var currentClassValue = element.className;
+
+          if (currentClassValue == classToRemove) {
+              element.className = "";
+              return;
+          }
+
+          var classValues = currentClassValue.split(" ");
+          var filteredList = [];
+
+          for (var i = 0 ; i < classValues.length; i++) {
+              if (classToRemove != classValues[i]) {
+                  filteredList.push(classValues[i]);
+              }
+          }
+
+          element.className = filteredList.join(" ");
+      }
